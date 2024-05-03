85d73e0424925e22bf61529fbce75ac1dfc07df0
amr <amrmasri999@gmail.com>
2024-05-03 19:24:55 +0300

    adding codes

--- counter/src/Counter.jsx
+++ counter/src/Counter.jsx
@@ -1,9 +1,30 @@
-
+import React, {useState} from 'react'; 
 
 function Counter(){
+
+    const [count , setCount] = useState(0);
+
+    const increment = () => {
+        setCount( count +1 );
+    }
+
+    const decrement = () => {
+        setCount (count -1)
+    }
+
+    const reset = () => {
+        setCount (0)
+    }
+
     return(
-        <p>Hello</p>
+        <div className="counter-container">
+            <p className='count-display'>{count}</p>
+            <button className='counter-button' onClick={decrement}>Decrement</button>
+            <button className='counter-button' onClick={reset}>reset</button>
+            <button className='counter-button' onClick={increment}>increment</button>
+        </div>
     );
+
 }
 
 export default Counter;
